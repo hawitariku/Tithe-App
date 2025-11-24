18a2bb66a873911b50e9c23c15a3946d19a7f5dd
Hawi Tariku <hawitariku628@gmail.com>
2025-11-24 14:03:39 +0200

    Add GitHub Actions workflows for building Tithe App and update project files

--- services/NotificationService.js
+++ services/NotificationService.js
@@ -1,5 +1,4 @@
 import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Configure notification handler
